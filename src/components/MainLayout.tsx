7571c3f699ac89f98ff0ed6d0e1ed64a5ab244ba
YudadikaP <intionto2@gmail.com>
2024-11-24 21:31:51 +0700

    Update MainLayout.tsx

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -13,7 +13,7 @@ import { scrollTo } from '../utils'
 import Script from 'next/script'
 
 const url = BASE_PATH
-const authorName = 'Irfan Maulana'
+const authorName = 'Yoeda Dika'
 
 interface LayoutProps {
   children: ReactChild
